ceff436e0bd7e6f569c38e9bdc05166f4d5073ce
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-03-13 23:29:39 +0900

    파이어베이스 로그인 로그아웃 인증 구현

--- app/src/main/java/com/chaeseong/mysampleapp/MainActivity.kt
+++ app/src/main/java/com/chaeseong/mysampleapp/MainActivity.kt
@@ -2,8 +2,11 @@ package com.chaeseong.mysampleapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.chaeseong.mysampleapp.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
 
@@ -11,15 +14,35 @@ class MainActivity : AppCompatActivity() {
 
 	private lateinit var auth: FirebaseAuth
 
+	private lateinit var binding: ActivityMainBinding
+
 	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
 
 		auth = FirebaseAuth.getInstance()
 
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
+
+		binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+
 		val joinBtnClicked = findViewById<Button>(R.id.joinBtn)
 		joinBtnClicked.setOnClickListener {
-			auth.createUserWithEmailAndPassword("abc@abc.com", "12341234")
+
+			// 첫번째 방법
+//			val email = findViewById<EditText>(R.id.emailArea)
+//			val pwd = findViewById<EditText>(R.id.pwdArea)
+
+			// 두번째 방법
+			val email = binding.emailArea
+			val pwd = binding.pwdArea
+
+			auth.createUserWithEmailAndPassword(
+				email.text.toString(),
+				pwd.text.toString())
 				.addOnCompleteListener(this) { task ->
 					if (task.isSuccessful) {
 						Toast.makeText(this,"ok",Toast.LENGTH_SHORT).show()
@@ -27,7 +50,30 @@ class MainActivity : AppCompatActivity() {
 						Toast.makeText(this,"fail",Toast.LENGTH_SHORT).show()
 					}
 				}
+		}
 
+		binding.logoutBtn.setOnClickListener {
+
+			auth.signOut()
+			Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
+		}
+
+		binding.loginBtn.setOnClickListener {
+
+			val email = binding.emailArea
+			val pwd = binding.pwdArea
+
+			auth.signInWithEmailAndPassword(
+				email.text.toString(),
+				pwd.text.toString()
+			).addOnCompleteListener(this) { task ->
+					if (task.isSuccessful) {
+						Toast.makeText(this,"ok",Toast.LENGTH_SHORT).show()
+						Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
+					} else {
+						Toast.makeText(this,"fail",Toast.LENGTH_SHORT).show()
+					}
+				}
 		}
 	}
 }
